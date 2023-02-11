dbb874fd7a56fd386ee5b8b573aa26418a5c751b
Eric Chen <eric.chen.us@gmail.com>
2023-02-11 18:59:10 -0500

    broke settings modal into two modals and added verification for paper publishing

--- components/PublishModal.js
+++ components/PublishModal.js
@@ -3,12 +3,13 @@ import { Dialog, Transition } from '@headlessui/react';
 import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 import { LockClosedIcon } from '@heroicons/react/20/solid';
 
-const PaperSettings = ({
+const PublishModal = ({
     setButtonClick,
     buttonClick,
     item,
     settingsClickId,
     setSettingsClickId,
+    privatePaperTitle = 'Example Title',
 }) => {
     // const [open, setOpen] = useState(true)
 
@@ -18,6 +19,12 @@ const PaperSettings = ({
     const [file, setFile] = useState('');
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [paperTitle, setPaperTitle] = useState('');
+    const [inputValue, setInputValue] = useState('');
+    const [title, setTitle] = useState(privatePaperTitle);
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
 
     const openModal = () => {
         setModalIsOpen(true);
@@ -103,40 +110,54 @@ const PaperSettings = ({
                                                 as='h3'
                                                 className='text-lg font-medium leading-6 text-gray-900 text-center'
                                             >
-                                                Paper Settings
+                                                Publish
                                             </Dialog.Title>
                                             {/* <h1>{item['title']}</h1>
                                             <h1>{item['author']}</h1> */}
+
                                             <div className='mt-2'>
                                                 <form
                                                     className='mt-8 space-y-10 '
                                                     onSubmit={handleSubmit}
                                                 >
+                                                    <h2>
+                                                        You cannot undo once you
+                                                        publish your paper.
+                                                        Please type in the name
+                                                        of the paper in order to
+                                                        confirm that you want to
+                                                        make the paper public.
+                                                    </h2>
                                                     {/* <input type="hidden" name="remember" defaultValue="true" /> */}
                                                     <div className=' rounded-md shadow-sm space-y-5 '>
                                                         <div>
-                                                            <h2>Add Readers</h2>
-                                                            <label
-                                                                htmlFor='authors'
-                                                                className='sr-only'
-                                                            >
-                                                                Add Readers
-                                                            </label>
-                                                            <input
-                                                                id='authors'
-                                                                name='authors'
-                                                                type='text'
-                                                                autoComplete='authors'
-                                                                required
-                                                                className='relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
-                                                                placeholder='abc@def.xyz'
-                                                                onChange={(e) =>
-                                                                    setEmail(
-                                                                        e.target
-                                                                            .value
-                                                                    )
-                                                                }
-                                                            />
+                                                            <div className='container mx-auto'>
+                                                                <input
+                                                                    id='confirm'
+                                                                    name='confirm'
+                                                                    type='text'
+                                                                    autoComplete='authors'
+                                                                    required
+                                                                    className='relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
+                                                                    placeholder='Enter paper title'
+                                                                    value={
+                                                                        inputValue
+                                                                    }
+                                                                    onChange={
+                                                                        handleInputChange
+                                                                    }
+                                                                />
+                                                                {/* <button
+                                                                    className='bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded'
+                                                                    id='submitButton'
+                                                                    disabled={
+                                                                        inputValue !==
+                                                                        title
+                                                                    }
+                                                                >
+                                                                    Submit
+                                                                </button> */}
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </form>
@@ -156,16 +177,23 @@ const PaperSettings = ({
                                     >
                                         Cancel
                                     </button>
+
                                     <button
                                         type='submit'
-                                        className='group relative flex w-1/4 justify-around rounded-md border border-transparent bg-orange-500 py-2 px-4 text-sm font-medium text-white hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+                                        className={`group relative flex w-1/4 justify-around rounded-md border border-transparent bg-orange-500 py-2 px-4 text-sm font-medium text-white hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ${
+                                            inputValue !== title
+                                                ? 'opacity-50 cursor-not-allowed'
+                                                : ''
+                                        }`}
+                                        id='submitButton'
+                                        disabled={inputValue !== title}
                                         onClick={() => {
                                             setButtonClick(false);
                                             setSettingsClickId(null);
                                         }}
                                         ref={cancelButtonRef}
                                     >
-                                        <span>Confirm</span>
+                                        Publish
                                     </button>
                                 </div>
                             </Dialog.Panel>
@@ -177,4 +205,4 @@ const PaperSettings = ({
     );
 };
 
-export default PaperSettings;
+export default PublishModal;
