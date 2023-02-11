cb4c3686b5e7a12741eafeffa354f30c6121e11e
Yiheng Chen <chenyihengc.y@hotmail.com>
2023-02-10 23:26:42 -0500

    feat: button integration. repo organization

--- pages/dashBoard.js
+++ pages/dashBoard.js
@@ -1,85 +1,78 @@
-import { useState } from "react";
-import { ContractABI } from '../components/contractABI.js';
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
+import Upload from '../components/Upload'
 
-export default function Home() {
+export default function dashboard() {
+  const [buttonClick, setButtonClick] = useState(false)
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-purple-700">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div class="px-4 pt-12 pb-32 mx-auto max-w-screen-x1 sm:px-6 lg:px-8">
+      <div class = "flex justify-evenly">
+        <div>
+          <button class="bg-gray-300 hover:bg-gray-400 text-black font-medium py-2 px-4 rounded-none">
+            My Papers
+          </button>
         </div>
-      </main>
+        <div>
+          <button class="bg-gray-100 hover:bg-gray-400 text-slate-600 font-medium py-2 px-4 rounded-none">
+            Shared With Me
+          </button>
+        </div>
+      </div>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+
+      <div class="relative ">
+        <button onClick={() => {
+                if (buttonClick === false) {
+                    setButtonClick(true)
+                }
+
+            }}class="bg-orange-500 hover:bg-orange-600 text-white font-medium py-2 px-4 rounded-none">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                </svg>
+
+          <span>Upload</span>
+        </button>
+        {buttonClick && <Upload buttonClick = {buttonClick} setButtonClick={setButtonClick} />}
+      </div>
+
+      <div>
+
+        <div class="mt-8 grid grid-cols-3 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          <div class = "bg-blue-100">
+            <Image 
+              src="/../public/papers.jpeg"
+              alt="papers image"
+              width={100}
+              height={100}
+            />
+            <div class="pt-4">
+              <p class="text-lg font-medium">Paper 1</p>
+              <p class="text-sm font-medium">Author:</p>
+              <p class="text-sm font-medium">Shared With:</p>
+            </div>
+            <div class="text-center text-xs p-5 bg-gray-100 rounded-full w-3/4">
+              Peer Reviewed By: Research Gate
+            </div>
+
+            <Link href="https://etherscan.io">
+              <p className="text-bg-gray-300 hover:text-cyan-600 underline transition hover:decoration-blue-400">
+                Open In Explorer
+              </p>
+            </Link>
+
+          </div>
+
+          
+        </div>
+
+        
+        
+
+      </div>
     </div>
   )
 }
