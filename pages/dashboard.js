820a3139c7e5885e2d38f22a41f1d1791c13ceb6
Spencer Solit <ssolit@gmail.com>
2023-02-11 14:50:39 -0500

    can adpatively add papers to grid

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,25 +2,68 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
+// import { json } from 'stream/consumers'
 import Upload from '../components/Upload'
 
-export default function dashboard() {
+export default function dashboard2() {
   const [buttonClick, setButtonClick] = useState(false)
 
-  const ex_json = ```{papers: [
-                        paperData: {"title" : "Title 1", 
-                                    "author" : "Author 1", 
-                                    "shared" : ["address 1.1"],
-                                    "reviewed" : "Reviewer 1",
-                                    "url" : "https://url1.com"
-                                  },
-                        paperData: {"title" : "Title 2", 
-                                  "author" : "Author 2", 
-                                  "shared" : ["address 2.1"],
-                                  "reviewed" : "Reviewer 2",
-                                  "url" : "https://url2.com"
-                                },
-                      ]}```;
+  const json_data = {papers: [
+                          {title : "Title 1", 
+                            author : "Author 1", 
+                            shared : ["address 1.1"],
+                            reviewed : "Reviewer 1",
+                            url : "https://url1.com"
+                          },
+                          {title : "Title 2", 
+                            author : "Author 2", 
+                            shared : ["address 2.1"],
+                            reviewed : "Reviewer 2",
+                            url : "https://url2.com"
+                          },
+                        ]};
+  const data_array = [
+    {title : "Title 1", 
+      author : "Author 1", 
+      shared : ["address 1.1"],
+      reviewed : "Reviewer 1",
+      url : "https://url1.com"
+    },
+    {title : "Title 2", 
+      author : "Author 2", 
+      shared : ["address 2.1"],
+      reviewed : "Reviewer 2",
+      url : "https://url2.com"
+    },
+  ];
+
+  function realDiv(item) {
+    return (
+      <div class = "bg-blue-100 rounded-t">
+        <Image class="rounded-t-lg"
+          src="/../public/papers.jpeg"
+          alt="papers image"
+          width={500}
+          height={500}
+        />
+        <div class="pt-4 mx-4">
+          <p class="text-lg font-semibold mb-2"> {item["title"]} </p>
+          <p class="text-sm font-medium">Author: {item["author"]}</p>
+          <p class="text-sm font-medium">Shared With: {item["shared"]}</p>
+          <div class="text-center text-xs p-1 bg-gray-100 rounded-full w-3/4 mt-2 mb-2">
+            Peer Reviewed By: {item["reviewed"]}
+          </div>
+        </div>
+        <Link href={item["url"]}>
+          <p className="text-bg-gray-300 hover:text-cyan-600 text-xs text-right underline transition hover:decoration-blue-400 m-1">
+            Open In Explorer
+          </p>
+        </Link>
+      </div>
+    )
+  }
+
+
 
   return (
     <div class="px-4 pt-12 pb-32 mx-auto max-w-screen-x1 sm:px-6 lg:px-8">
@@ -38,7 +81,7 @@ export default function dashboard() {
       </div>
 
 
-      <div class="relative ">
+      <div class="relative">
         <button onClick={() => {
                 if (buttonClick === false) {
                     setButtonClick(true)
@@ -56,34 +99,8 @@ export default function dashboard() {
 
       <div>
         <div class="mt-8 grid grid-cols-3 gap-8 md:grid-cols-2 lg:grid-cols-3 drop-shadow-lg">
-          <div class = "bg-blue-100 rounded-t">
-            <Image class="rounded-t-lg"
-              src="/../public/papers.jpeg"
-              alt="papers image"
-              width={500}
-              height={500}
-            />
-            <div class="pt-4 mx-4">
-              <p class="text-lg font-semibold mb-2">Paper 1</p>
-              <p class="text-sm font-medium">Author:</p>
-              <p class="text-sm font-medium">Shared With:</p>
-              <div class="text-center text-xs p-1 bg-gray-100 rounded-full w-3/4 mt-2 mb-2">
-                Peer Reviewed By: Research Gate
-              </div>
-            </div>
-            <Link href="https://etherscan.io">
-              <p className="text-bg-gray-300 hover:text-cyan-600 text-xs text-right underline transition hover:decoration-blue-400 m-1">
-                Open In Explorer
-              </p>
-            </Link>
-          </div>
-
-          
+          {data_array.map(realDiv)}
         </div>
-
-        
-        
-
       </div>
     </div>
   )
